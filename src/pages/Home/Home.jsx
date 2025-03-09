e1a7b6c004341d8aa15609280f800fb73bd63745
maruyaya <115956361+maruyaya@users.noreply.github.com>
2025-03-08 23:08:23 -0700

    more homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,26 @@ import homepage from "/images/Homepage.jpg";
 export const Home = () => {
     return (
         <div className={styles.container}>
-           <h1 className={styles.title}>Find your matcha</h1>
-           {/*<img src={homepage}></img>*/}
+           <div className={styles.title}>
+                <h1>find your matcha</h1>
+           </div>
+           <br></br>
+            <div className={styles.textContainer}>
+                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam minus voluptatum velit quae asperiores, labore esse iste mollitia consequatur praesentium corrupti eum suscipit, fugiat, harum aliquid necessitatibus magni quasi reprehenderit!</p>
+            </div>
+            <div className={styles.buttonsContainer}>
+                <ul className={styles.buttons}>
+                    <li>
+                        <Link to="/link">log in</Link>
+                        </li>
+                    </ul>
+                <ul className={styles.buttons}>
+                    <li>
+                        <Link to="/register"> create an account</Link>
+                    </li>
+                </ul>
+            </div>
+           
         </div>
 
     )
