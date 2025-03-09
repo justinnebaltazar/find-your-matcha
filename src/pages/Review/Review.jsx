05f8fb9e6995e26f82557f167029bc4f9c9e96f6
aleung30 <aleung30@student.ubc.ca>
2025-03-08 21:21:34 -0800

    Updated review page

--- src/pages/Review/Review.jsx
+++ src/pages/Review/Review.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import styles from "./Review.module.css";
+import rec from "/public/images/rec.jpg";
 
 const Review = () => {
   return (
-    <div>Review</div>
-  )
-}
+
+
+    <section className={styles.review}>
+        <form>
+            <h2>leave a review</h2>
+            <div className={styles.inputbox}>
+                <label>name</label>
+                <input type="text" className="field" placeholder='please enter your name' required />
+                </div>
+                <div className={styles.inputbox}>
+                <label>message</label>
+                <textarea name="" id="" placeholder='enter your message'></textarea>
+                </div>
+                <button type={styles.submit}>submit</button>
+            
+        </form>
+        
+    </section>
+   
+  );
+};
 
 export default Review
+
+
+
